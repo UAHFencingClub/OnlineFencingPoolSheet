81ec0d38eca26857fda0f22605f1596fc11c5add
Erik Fong <quantumerikfong@gmail.com>
2024-07-28 20:46:51 -0400

    Initial layout planning

--- src/components/pool_sheet/mod.rs
+++ src/components/pool_sheet/mod.rs
@@ -15,163 +15,44 @@ use serde_json;
 #[component]
 pub fn PoolSheet(fencers: ReadSignal<Vec<String>>) -> impl IntoView {
     view! {
-        {move || {
-            let fencers: Vec<SimpleFencer> = fencers
-                .get()
-                .into_iter()
-                .map(|fencer_str| { SimpleFencer::new(fencer_str) })
-                .collect();
-            let (poolsheet_sig, set_poolsheet_sig) = create_signal(
-                PoolSheet::new(fencers, &SimpleBoutsCreator).map_or_else(|_| None, |x| Some(x)),
-            );
-            view! {
-                <div>
-                    <div class="poolsheet">
-                        <table>
-                            <tr>
-                                <td></td>
-                                {poolsheet_sig
-                                    .with(|poolsheet| {
-                                        match poolsheet {
-                                            Some(sheet) => {
-                                                Some(
-                                                    sheet
-                                                        .get_fencers()
-                                                        .iter()
-                                                        .map(|fencer| {
-                                                            view! {
-                                                                <td class="pool-sheet-fencer-second">
-                                                                    {fencer.get_fullname()}
-                                                                </td>
-                                                            }
-                                                        })
-                                                        .collect::<Vec<_>>(),
-                                                )
-                                            }
-                                            None => None,
-                                        }
-                                    })}
+        <div id="pool-sheet">
+            {move || {
+                let fencers: Vec<SimpleFencer> = fencers
+                    .get()
+                    .into_iter()
+                    .map(|fencer_str| { SimpleFencer::new(fencer_str) })
+                    .collect();
+                let (poolsheet_sig, set_poolsheet_sig) = create_signal(
+                    PoolSheet::new(fencers, &SimpleBoutsCreator).ok(),
+                );
+                poolsheet_sig
+                    .with(|poolsheet_option| {
+                        match poolsheet_option {
+                            Some(poolsheet) => view! { "Hello Sheet" },
+                            None => view! { "Goodbye Sheet" },
+                        }
+                    })
+            }}
 
-                            </tr>
-                            {poolsheet_sig
-                                .with(|poolsheet| {
-                                    poolsheet
-                                        .as_ref()
-                                        .map(|sheet| {
-                                            sheet
-                                                .get_fencers()
-                                                .iter()
-                                                .map(|fencer_main| {
-                                                    view! {
-                                                        <tr>
-                                                            <td class="pool-sheet-fencer">
-                                                                {fencer_main.get_fullname()}
-                                                            </td>
-                                                            {poolsheet_sig
-                                                                .with(|poolsheet| {
-                                                                    poolsheet
-                                                                        .as_ref()
-                                                                        .expect("Checked Earlier")
-                                                                        .get_fencers()
-                                                                        .iter()
-                                                                        .map(|fencer_second| {
-                                                                            view! {
-                                                                                <td
-                                                                                    class=if fencer_second == fencer_main {
-                                                                                        "pool-sheet-score-box-null"
-                                                                                    } else {
-                                                                                        "pool-sheet-score-box"
-                                                                                    }
-
-                                                                                    id=format!(
-                                                                                        "{}-{}",
-                                                                                        fencer_main.get_fullname(),
-                                                                                        fencer_second.get_fullname(),
-                                                                                    )
-                                                                                >
-
-                                                                                    {{
-                                                                                        FencerVs::new(
-                                                                                                fencer_main.to_owned().to_owned(),
-                                                                                                fencer_second.to_owned().to_owned(),
-                                                                                            )
-                                                                                            .ok()
-                                                                                            .map(|y| {
-                                                                                                let x = poolsheet_sig
-                                                                                                    .with(|poolsheet| {
-                                                                                                        poolsheet
-                                                                                                            .as_ref()
-                                                                                                            .expect("Checked Earlier")
-                                                                                                            .get_bout(&y)
-                                                                                                            .unwrap()
-                                                                                                            .get_score(fencer_main)
-                                                                                                            .unwrap()
-                                                                                                    });
-                                                                                                x
-                                                                                            })
-                                                                                    }}
-
-                                                                                </td>
-                                                                            }
-                                                                        })
-                                                                        .collect::<Vec<_>>()
-                                                                })}
-
-                                                        </tr>
-                                                    }
-                                                })
-                                                .collect::<Vec<_>>()
-                                        })
-                                })}
-
-                        </table>
-                    </div>
-                    <ol class="bout-list">
-                        {poolsheet_sig
-                            .with(|poolsheet| {
-                                match poolsheet {
-                                    Some(sheet) => {
-                                        Some(
-                                            sheet
-                                                .iter_bouts()
-                                                .map(|(versus, _)| {
-                                                    let (bout_score_a, set_bout_score_a) = create_signal(None);
-                                                    let (bout_score_b, set_bout_score_b) = create_signal(None);
-                                                    view! {
-                                                        <li>
-                                                            {format!(
-                                                                "{} vs {}",
-                                                                versus.0.get_fullname(),
-                                                                versus.1.get_fullname(),
-                                                            )}
-                                                            <input
-                                                                type="number"
-                                                                on:input=move |ev| {
-                                                                    let test = event_target_value(&ev).parse::<u8>().ok();
-                                                                    set_bout_score_a(test);
-                                                                }
-                                                            />
-                                                            <input
-                                                                type="number"
-                                                                on:input=move |ev| {
-                                                                    let test = event_target_value(&ev).parse::<u8>().ok();
-                                                                    set_bout_score_b(test);
-                                                                }
-                                                            />
-                                                            <p>--- {bout_score_a} - {bout_score_b}</p>
-                                                        </li>
-                                                    }
-                                                })
-                                                .collect::<Vec<_>>(),
-                                        )
-                                    }
-                                    None => None,
-                                }
-                            })}
-
-                    </ol>
-                </div>
-            }
-        }}
+        </div>
     }
 }
+
+#[component]
+pub fn PoolSheetTable() -> impl IntoView {}
+
+#[component]
+fn BoutList() -> impl IntoView {}
+
+#[component]
+pub fn BoutScoreTableCell(
+    main_fencer: SimpleFencer,
+    secondary_fencer: SimpleFencer,
+) -> impl IntoView {
+}
+
+#[component]
+pub fn PoolTableHeader() -> impl IntoView {}
+
+#[component]
+pub fn PoolFencerTableRow(children: Children) -> impl IntoView {}
