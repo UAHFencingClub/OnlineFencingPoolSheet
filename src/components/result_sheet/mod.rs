254fd993c6165e82ff416a547f3179d29bc2e33b
Erik Fong <quantumerikfong@gmail.com>
2024-08-03 00:59:43 -0400

    Table display for results.

--- src/components/result_sheet/mod.rs
+++ src/components/result_sheet/mod.rs
@@ -1,15 +1,39 @@
-use fencing_sport_lib::{fencer::SimpleFencer, pools::PoolResults};
+use fencing_sport_lib::{
+    fencer::{Fencer, SimpleFencer},
+    pools::PoolResults,
+};
 use leptos::*;
 
 #[component]
 pub fn PoolResultTable(pool_results: PoolResults<SimpleFencer>) -> impl IntoView {
     view! {
-        <ol>
+        <table>
+            <tr>
+                <th></th>
+                <th>"Fencer"</th>
+                <th>TS</th>
+                <th>TR</th>
+                <th>Ind</th>
+                <th>V</th>
+                <th>Pl</th>
+            </tr>
             {pool_results
                 .iter()
                 .enumerate()
-                .map(|(index, result)| view! { <li>{index} - {format!("{:?}", result.1)}</li> })
+                .map(|(index, result)| {
+                    view! {
+                        <tr>
+                            <td>{index}</td>
+                            <td>{result.1.fencer().get_fullname()}</td>
+                            <td>{*result.1.touches_scored()}</td>
+                            <td>{*result.1.touches_recieved()}</td>
+                            <td>{*result.1.indicator()}</td>
+                            <td>{*result.1.victories()}</td>
+                            <td>{*result.1.place()}</td>
+                        </tr>
+                    }
+                })
                 .collect_view()}
-        </ol>
+        </table>
     }
 }
