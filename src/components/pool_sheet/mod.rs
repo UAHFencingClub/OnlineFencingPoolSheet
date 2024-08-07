c66b7555c314770d4a0e7e45146c6833697fafd4
Erik Fong <quantumerikfong@gmail.com>
2024-08-07 02:32:31 -0400

    Redid the signaling to put logic closer to where it is called ( I think that makes more sense organizationally with how things are arranged)
    Use newer functionality of the fencing-sport-lib to fix bugs

--- src/components/pool_sheet/mod.rs
+++ src/components/pool_sheet/mod.rs
@@ -23,49 +23,47 @@ where
 
     match option_poolsheet {
         Ok(poolsheet) => {
-            let (poolsheet_sig, set_poolsheet_sig) = create_signal(poolsheet);
+            let poolsheet_signals = create_signal(poolsheet);
 
-            let set_bout_score = move |fencer_a, fencer_b| {
-                set_poolsheet_sig.update(|poolsheet| {
-                    poolsheet.update_score(fencer_a, fencer_b).ok();
-                })
-            };
+            // let set_bout_score = move |fencer_a, fencer_b| {
+            //     set_poolsheet_sig.update(|poolsheet| {
+            //         poolsheet.update_score(fencer_a, fencer_b).ok();
+            //     })
+            // };
             let get_fencers = move || {
-                poolsheet_sig
+                poolsheet_signals
+                    .0
                     .with(|sheet| sheet.get_fencers().into_iter().cloned().collect::<Vec<_>>())
             };
-            let get_bout_score = move |fencer_vs| {
-                poolsheet_sig.with(|sheet| {
-                    let bout = sheet.get_bout(&fencer_vs).ok()?;
-                    bout.get_scores()
-                })
-            };
-            let get_bout_main_score = move |fencer_main: SimpleFencer, fencer_sec: SimpleFencer| {
-                info!("Getting main score for {fencer_main:?} {fencer_sec:?}");
-                poolsheet_sig.with(|sheet| {
-                    let vs = FencerVs::new(fencer_main.clone(), fencer_sec.clone()).unwrap();
-                    let bout = sheet.get_bout(&vs).unwrap();
-                    bout.get_score(fencer_main)
-                })
-            };
+            // let get_bout_score = move |fencer_vs| {
+            //     poolsheet_sig.with(|sheet| {
+            //         let bout = sheet.get_bout(&fencer_vs).ok()?;
+            //         bout.get_scores()
+            //     })
+            // };
+            // let get_bout_main_score = move |fencer_main: SimpleFencer, fencer_sec: SimpleFencer| {
+            //     info!("Getting main score for {fencer_main:?} {fencer_sec:?}");
+            //     poolsheet_sig.with(|sheet| {
+            //         let vs = FencerVs::new(fencer_main.clone(), fencer_sec.clone()).unwrap();
+            //         let bout = sheet.get_bout(&vs).unwrap();
+            //         bout.get_score(fencer_main)
+            //     })
+            // };
             let get_versus = move || {
-                poolsheet_sig.with(|sheet| {
+                poolsheet_signals.0.with(|sheet| {
                     sheet
                         .iter_bouts()
                         .map(|(vs, _)| (vs.clone()))
                         .collect::<Vec<_>>()
                 })
             };
-            let versus = get_versus();
+            // let versus = get_versus();
             view! {
-                <PoolSheetTable fencers=get_fencers get_main_score=get_bout_main_score/>
-                <BoutList
-                    versus=versus
-                    set_score_closure=set_bout_score
-                    get_score_closure=get_bout_score
-                />
+                <PoolSheetTable fencers=get_fencers poolsheet_sigs=poolsheet_signals/>
+                <BoutList versus=get_versus() poolsheet_sigs=poolsheet_signals/>
                 <button on:click=move |_| {
-                    poolsheet_sig
+                    poolsheet_signals
+                        .0
                         .with(|sheet| {
                             if sheet.is_finished() {
                                 on_complete(sheet.finish().unwrap())
