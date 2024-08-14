2bdbfc8924d46ef7bded5b9c19005108bbdfda8d
Erik Fong <quantumerikfong@gmail.com>
2024-08-13 22:26:57 -0400

    explicitly mount the fencer list into a different element for later styling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,62 @@
+// use fencing_pool_sheet_leptos::App;
 use leptos::*;
-use fencing_pool_sheet_leptos::App;
+
+use fencing_pool_sheet_leptos::components::{
+    fencer_list::{FencerList, FencerListError},
+    pool_sheet::PoolSheet,
+    result_sheet::PoolResultTable,
+};
+use leptos::wasm_bindgen::JsCast;
+
+use fencing_sport_lib::{
+    fencer::SimpleFencer,
+    pools::{PoolResults, PoolSheetError},
+};
 
 fn main() {
     // set up logging
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
-    mount_to_body(|| {
+    let (competiors, set_competitors) = create_signal(Err(FencerListError::NoFencers));
+    let (results, set_results) = create_signal(
+        Result::<PoolResults<SimpleFencer>, PoolSheetError>::Err(PoolSheetError::PoolNotComplete),
+    );
+
+    let doc = leptos::document();
+    let fencer_main_div = doc
+        .get_element_by_id("fencer-list-main-div")
+        .expect("Element should exist in the index.html");
+
+    mount_to(fencer_main_div.clone().unchecked_into(), move || {
+        view! { <FencerList submit_fencers=set_competitors/> }
+    });
+
+    mount_to_body(move || {
         view! {
-            <App />
+            {move || {
+                match competiors.get() {
+                    Ok(fencers) => {
+                        view! {
+                            <div class="poolsheet-container">
+                                <PoolSheet
+                                    fencers=fencers
+                                    on_complete=move |results| { set_results.set(results) }
+                                />
+                            </div>
+                        }
+                            .into_view()
+                    }
+                    Err(err) => view! { <p>{format!("{err:?}")}</p> }.into_view(),
+                }
+            }}
+
+            {move || {
+                match results.get() {
+                    Ok(results) => view! { <PoolResultTable pool_results=results/> }.into_view(),
+                    Err(err) => view! { <p>{format!("{err:?}")}</p> }.into_view(),
+                }
+            }}
         }
     })
 }
